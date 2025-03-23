e1c959bab0c377d07fcd087d6891d4164d42c92b
Dialyn Santiago <dialynsantiago@macbookpro.mynetworksettings.com>
2025-03-23 17:58:51 -0400

    Added Neotree and keymap

--- nvim/init.lua
+++ nvim/init.lua
@@ -37,7 +37,19 @@ require("lazy").setup({
 	-- or                              , branch = '0.1.x',
 		  dependencies = { 'nvim-lua/plenary.nvim' }
 	},
-	{"nvim-treesitter/nvim-treesitter", build = ":TSUpdate"}
+	{
+		 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
+	},
+	{
+	  "nvim-neo-tree/neo-tree.nvim",
+	  branch = "v3.x",
+	  dependencies = {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+		"MunifTanjim/nui.nvim",
+		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
+	  		}
+	  }
   },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
@@ -49,6 +61,8 @@ require("lazy").setup({
 local builtin = require("telescope.builtin")
 vim.keymap.set('n', '<C-p>', builtin.find_files, {})
 vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+
+vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
 
 local config = require("nvim-treesitter.configs")
 config.setup({
