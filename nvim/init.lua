9af0b016735673d48c11a85288f455cd05266dac
Dialyn Santiago <dialynsantiago@macbookpro.mynetworksettings.com>
2025-03-23 19:50:54 -0400

    Organized lua plugin files and added lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -23,57 +23,9 @@ vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
 -- Setup lazy.nvim
-require("lazy").setup({
-  spec = {
-    -- add your plugins here
-		{
-	  "folke/tokyonight.nvim",
-	  lazy = false,
-	  priority = 1000,
-	  opts = {},
-	},
-	{
-		'nvim-telescope/telescope.nvim', tag = '0.1.8',
-	-- or                              , branch = '0.1.x',
-		  dependencies = { 'nvim-lua/plenary.nvim' }
-	},
-	{
-		 'nvim-treesitter/nvim-treesitter', build = ':TSUpdate'
-	},
-	{
-	  "nvim-neo-tree/neo-tree.nvim",
-	  branch = "v3.x",
-	  dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-		"MunifTanjim/nui.nvim",
-		-- {"3rd/image.nvim", opts = {}}, -- Optional image support in preview window: See `# Preview Mode` for more information
-	  		}
-	  }
-  },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})  
+require("lazy").setup("plugins")  
 
-local builtin = require("telescope.builtin")
-vim.keymap.set('n', '<C-p>', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 
-vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<CR>', {})
-
-local config = require("nvim-treesitter.configs")
-config.setup({
-	ensure_installed = { "c", "lua", "vim", "vimdoc", "javascript", "html" },
-	sync_install = false,
-	highlight = { enable = true },
-	indent = { enable = true },  
-})
-
-require("tokyonight").setup()
-vim.cmd.colorscheme "tokyonight"
 
 vim.cmd("set tabstop=4")
 vim.cmd("set shiftwidth=4")
